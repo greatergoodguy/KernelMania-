95f4cd3d16d7d3a84fa25e7cead2956ff964e3f0
Tom Yu <tom.yu@burstingbrains.com>
2014-01-11 12:45:03 -0600

    Added Timer

--- Assets/_Bursting Brains/Scripts/Controllers/CtrlUIGameplay.cs
+++ Assets/_Bursting Brains/Scripts/Controllers/CtrlUIGameplay.cs
@@ -3,22 +3,70 @@ using System.Collections;
 
 public class CtrlUIGameplay : MonoBehaviour {
 	
-	private static readonly string LABEL_SCORE = "Score: ";
+	private static readonly string LABEL_SCORE = "Score   ";
+	private static readonly string LABEL_TIMER = "Timer   ";
 	
-	tk2dTextMesh scoreTM;
+	//private static readonly float TIMER_VALUE_DEFAULT_IN_SECONDS = 180f;
+	private static readonly float TIMER_VALUE_DEFAULT_IN_SECONDS = 10f;
 	
+	tk2dTextMesh scoreTextTM;
 	int totalScore;
 	
+	tk2dTextMesh timerTextTM;
+	float timerValueInSeconds;
+	bool isTimerOn;
+	
 	void Awake() {
-		scoreTM = transform.FindChild_BB("Score Text").GetComponent_BB<tk2dTextMesh>();
+		timerTextTM = transform.FindChild_BB("Timer Text").GetComponent_BB<tk2dTextMesh>();
+		timerValueInSeconds = 0;
+		isTimerOn = false;
 		
+		scoreTextTM = transform.FindChild_BB("Score Text").GetComponent_BB<tk2dTextMesh>();
 		totalScore = 0;
+		
+		
+		TimerSet(TIMER_VALUE_DEFAULT_IN_SECONDS);
+		TimerStart();
+	}
+	
+	void Update() {
+		if(isTimerOn && timerValueInSeconds >= 0) {
+			timerValueInSeconds -= Time.deltaTime;
+			TimerSet(timerValueInSeconds);
+		}
 	}
 	
 	public void IncreaseScore(int addedScore) {
 		totalScore += addedScore;
 		
-		scoreTM.text = LABEL_SCORE + totalScore;
-        scoreTM.Commit();
+		scoreTextTM.text = LABEL_SCORE + totalScore;
+        scoreTextTM.Commit();
+	}
+	
+	public void TimerSet(float timerValueInSeconds) {
+		this.timerValueInSeconds = timerValueInSeconds;
+		
+		int remainingSeconds = (int) timerValueInSeconds;
+		
+		int remainingMinutes = remainingSeconds  / 60;
+		remainingSeconds -= remainingMinutes * 60;
+		
+		int remainingMilliseconds = (int) ((timerValueInSeconds % 1)  * 100);
+		
+		string answer = string.Format("{0:D1}:{1:D2}:{2:D2}", 
+    			remainingMinutes, 
+    			remainingSeconds,
+				remainingMilliseconds);
+		
+		timerTextTM.text = LABEL_TIMER + answer;
+        timerTextTM.Commit();
+	}
+	
+	public void TimerStart() {
+		isTimerOn = true;
+	}
+	
+	public void TimerStop() {
+		isTimerOn = false;
 	}
 }
